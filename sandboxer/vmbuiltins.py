c769084ca9dc3b5087106e61151ded048ff27f9a
Ivanq <imachug@yandex.ru>
2018-05-30 20:38:16 +0300

    Spaces, not tabs

--- sandboxer/vmbuiltins.py
+++ sandboxer/vmbuiltins.py
@@ -1,22 +1,22 @@
 class BuiltinNone(object):
-	pass
+    pass
 
 def setBuiltins(scope0):
-	scope0.inherits["help"]        = lambda: None
-	scope0.inherits["copyright"]   = lambda: None
-	scope0.inherits["credits"]     = lambda: None
-	scope0.inherits["license"]     = lambda: None
+    scope0.inherits["help"]        = lambda: None
+    scope0.inherits["copyright"]   = lambda: None
+    scope0.inherits["credits"]     = lambda: None
+    scope0.inherits["license"]     = lambda: None
 
-	scope0.inherits["__package__"] = "0background"
-	scope0.inherits["__name__"]    = "0background"
-	scope0.inherits["__doc__"]     = ""
-	scope0.inherits["__debug__"]   = False
+    scope0.inherits["__package__"] = "0background"
+    scope0.inherits["__name__"]    = "0background"
+    scope0.inherits["__doc__"]     = ""
+    scope0.inherits["__debug__"]   = False
 
-	def globals():
-		return scope0["locals"]()
-	scope0.inherits["globals"] = globals
+    def globals():
+        return scope0["locals"]()
+    scope0.inherits["globals"] = globals
 
-	def reload():
-		raise NotImplementedError("reload() is not supported")
+    def reload():
+        raise NotImplementedError("reload() is not supported")
 
-	#arr = ['input', 'print', 'open', 'raw_input', 'compile', '__import__', 'file', 'execfile', 'eval']
+    #arr = ['input', 'print', 'open', 'raw_input', 'compile', '__import__', 'file', 'execfile', 'eval']
