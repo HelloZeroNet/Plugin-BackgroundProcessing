6c4fee722cd781cc2b24c5a42b2634d203988f7e
Ivanq <imachug@yandex.ru>
2018-05-30 16:42:33 +0300

    import command

--- sandboxer/runtime.py
+++ sandboxer/runtime.py
@@ -1,2 +1,28 @@
+import importlib
+
 class Scope(object):
-	pass
+    def __init__(self):
+        self.vars = {}
+
+    def import_(self, names, from_, level):
+        for name, asname in names:
+            if asname is None:
+                asname = name
+
+            if from_ is not None:
+                line = "from %s import %s as import_module" % (from_, name)
+            else:
+                line = "import %s as import_module" % name
+
+            exec compile(line, "<import>", "single")
+            self[asname] = import_module
+            del import_module
+
+
+    def __getitem__(self, name):
+        if name in self.vars:
+            return self.vars[name]
+        raise NameError(name)
+
+    def __setitem__(self, name, value):
+        self.vars[name] = value
