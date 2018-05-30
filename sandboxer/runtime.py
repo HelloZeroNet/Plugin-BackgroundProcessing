820afdc6e4cc1d2c41a59d0455845dbf2427f472
Ivanq <imachug@yandex.ru>
2018-05-30 19:32:33 +0300

    More runtime stuff

--- sandboxer/runtime.py
+++ sandboxer/runtime.py
@@ -62,3 +62,24 @@ def populateScope(scope):
     scope["None"] = None
     scope["NotImplemented"] = NotImplemented
     scope["Ellipsis"] = Ellipsis
+
+    # Types
+    types = [
+        "bytearray", "unicode", "memoryview", "dict", "list", "set", "bytes",
+        "slice", "frozenset", "float", "basestring", "long", "type", "tuple",
+        "reversed", "str", "int", "complex", "bool", "buffer", "object",
+    ]
+    for type_name in types:
+        scope[type_name] = eval(type_name)  # Couldn't find a better way
+
+    # Secure default functions
+    funcs = [
+        "oct", "bin", "format", "repr", "sorted", "iter", "round", "dir", "cmp",
+        "reduce", "intern", "issubclass", "sum", "getattr", "abs", "hash",
+        "len", "ord", "super", "filter", "range", "staticmethod", "pow",
+        "divmod", "enumerate", "apply", "zip", "hex", "next", "chr", "xrange",
+        "hasattr", "delattr", "setattr", "property", "coerce", "unichr", "id",
+        "min", "any", "map", "max", "callable", "classmethod"
+    ]
+    for func_name in funcs:
+        scope[func_name] = eval(func_name)  # Couldn't find a better way
