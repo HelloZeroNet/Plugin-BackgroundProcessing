82bfe9b8f67038c97a5005a1d602b307aed44d8c
Ivanq <imachug@yandex.ru>
2019-05-22 19:06:07 +0300

    Add Crypto.ex for non-Bitcoin-only cryptography functions

--- crypt.py
+++ crypt.py
@@ -1,14 +1,29 @@
 from Crypt import CryptBitcoin
 
+try:
+	from Crypt import Cryptography
+except ImportError:
+	Cryptography = None
+
+
 allowed_names = (
 	"newPrivatekey", "newSeed", "hdPrivatekey", "privatekeyToAddress", "sign",
 	"verify"
 )
 
 def module(io):
+	class ExtendedCryptography:
+		def __init__(self):
+			for name in allowed_names:
+				setattr(self, name, getattr(Cryptography, name))
+
 	class Crypt:
 		def __init__(self):
 			for name in allowed_names:
 				setattr(self, name, getattr(CryptBitcoin, name))
+			if Cryptography is None:
+				self.ex = None
+			else:
+				self.ex = ExtendedCryptography()
 
 	return Crypt()
